b5c89b44d57c0a03261ffa5bf963771a6a43650d
unknown <Sebastian.Oraczek@microsoft.wsei.edu.pl>
2022-06-18 19:32:30 +0200

    Fetch custom data

--- Lab9/Lab9_WPF/Lab9_WPF/MainWindow.xaml.cs
+++ Lab9/Lab9_WPF/Lab9_WPF/MainWindow.xaml.cs
@@ -1,17 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.IO;
+using System.Net;
+using System.Net.Mime;
 using System.Text;
-using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace Lab9_WPF
 {
@@ -23,6 +19,71 @@ namespace Lab9_WPF
         public MainWindow()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        PageReposne reponse = FetchSnippets(1, 5, "java");
+
+        public void LoadData() => this.gridSnippet.ItemsSource = reponse.Batches;
+
+        public static string FetchData(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                ContentType type = new ContentType(response.ContentType ?? "text/plain;charset=" + Encoding.UTF8.WebName);
+                Encoding encoding = Encoding.GetEncoding(type.CharSet ?? Encoding.UTF8.WebName);
+
+                using (Stream stream = response.GetResponseStream())
+                using (StreamReader reader = new StreamReader(stream, encoding))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+
+        public static PageReposne FetchSnippets(int pageNumber, int pageSize, string snippetsType)
+        {
+            string url = $"https://dirask.com/api/snippets?pageNumber={pageNumber}&pageSize={pageSize}&dataOrder=newest&dataGroup=batches&snippetsType={Uri.EscapeUriString(snippetsType)}";
+            string data = FetchData(url);
+
+            return JsonSerializer.Deserialize<PageReposne>(data);
         }
     }
+}
+public class PageReposne
+{
+    [JsonPropertyName("pageNumber")]
+    public int PageNumber { get; set; }
+
+    [JsonPropertyName("pagesCount")]
+    public int PagesCount { get; set; }
+
+    [JsonPropertyName("pageSize")]
+    public int PageSize { get; set; }
+
+    [JsonPropertyName("totalCount")]
+    public int TotalCount { get; set; }
+
+    [JsonPropertyName("batches")]
+    public List<SnippetReponse> Batches { get; set; }
+}
+
+public class SnippetReponse
+{
+    [JsonPropertyName("size")]
+    public int Size { get; set; }
+
+    [JsonPropertyName("type")]
+    public string Type { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("creationTime")]
+    public DateTime? CreationTime { get; set; }
+
+    [JsonPropertyName("updateTime")]
+    public DateTime? UpdateTime { get; set; }
 }
