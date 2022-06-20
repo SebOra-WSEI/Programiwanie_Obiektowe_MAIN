529b8951582827bc74cc2566d7075b9c90160ef0
SebOra-WSEI <101014313+SebOra-WSEI@users.noreply.github.com>
2022-06-20 19:52:55 +0200

    New functionality

--- Lab9/Lab9_WPF/Lab9_WPF/MainWindow.xaml.cs
+++ Lab9/Lab9_WPF/Lab9_WPF/MainWindow.xaml.cs
@@ -1,30 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Mime;
 using System.Text;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.Windows;
 
 
 namespace Lab9_WPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        public static string Type { get; set; } = "";
+        public static int PageSize { get; set; } = 5;
+        public static int PageNumber { get; set; } = 1;
+
         public MainWindow()
         {
             InitializeComponent();
             LoadData();
         }
+        public void LoadData() =>
+            this.gridSnippet.ItemsSource = FetchSnippets(PageNumber, PageSize, Type).Batches;
 
-        PageReposne reponse = FetchSnippets(1, 5, "java");
+        // Handle type change
+        public void ChangeType(string type)
+        {
+            Type = type;
+            LoadData();
+        }
 
-        public void LoadData() => this.gridSnippet.ItemsSource = reponse.Batches;
+        private void Btn_Text_Click(object sender, RoutedEventArgs e) => ChangeType(Types.Text);
+
+        private void Btn_Bash_Click(object sender, RoutedEventArgs e) => ChangeType(Types.Bash);
+
+        private void Btn_Cpp_Click(object sender, RoutedEventArgs e) => ChangeType(Types.Cpp);
+
+        private void Btn_CSharp_Click(object sender, RoutedEventArgs e) => ChangeType(Types.CSharp);
+
+        private void Btn_Java_Click(object sender, RoutedEventArgs e) => ChangeType(Types.Java);
+
+        private void Btn_CSS_Click(object sender, RoutedEventArgs e) => ChangeType(Types.CSS);
+
+        private void Btn_HTML_Click(object sender, RoutedEventArgs e) => ChangeType(Types.HTML);
+
+        private void Btn_JS_Click(object sender, RoutedEventArgs e) => ChangeType(Types.JavaScript);
+
+        private void Btn_PHP_Click(object sender, RoutedEventArgs e) => ChangeType(Types.PHP);
+
+        private void Btn_Python_Click(object sender, RoutedEventArgs e) => ChangeType(Types.Python);
+
+        private void Btn_SQL_Click(object sender, RoutedEventArgs e) => ChangeType(Types.SQL);
+
+        // Handle Page Size change
+        public void Btn_LoadPageSizeData(object sender, RoutedEventArgs e)
+        {
+            PageSize = int.Parse(this.comboBoxPageSize.SelectedValue.ToString());
+            LoadData();
+        }
+
+
+        // Handle Page Number change
 
         public static string FetchData(string url)
         {
@@ -50,40 +86,10 @@ namespace Lab9_WPF
 
             return JsonSerializer.Deserialize<PageReposne>(data);
         }
+
+        private void Btn_One_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
-}
-public class PageReposne
-{
-    [JsonPropertyName("pageNumber")]
-    public int PageNumber { get; set; }
-
-    [JsonPropertyName("pagesCount")]
-    public int PagesCount { get; set; }
-
-    [JsonPropertyName("pageSize")]
-    public int PageSize { get; set; }
-
-    [JsonPropertyName("totalCount")]
-    public int TotalCount { get; set; }
-
-    [JsonPropertyName("batches")]
-    public List<SnippetReponse> Batches { get; set; }
-}
-
-public class SnippetReponse
-{
-    [JsonPropertyName("size")]
-    public int Size { get; set; }
-
-    [JsonPropertyName("type")]
-    public string Type { get; set; }
-
-    [JsonPropertyName("name")]
-    public string Name { get; set; }
-
-    [JsonPropertyName("creationTime")]
-    public DateTime? CreationTime { get; set; }
-
-    [JsonPropertyName("updateTime")]
-    public DateTime? UpdateTime { get; set; }
 }
